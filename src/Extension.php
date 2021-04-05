d05a73409514d9f78e4021e7f032f25131b2f3df
unknown <onlinewebdev@outlook.com>
2021-04-05 04:24:23 +0300

    add populate course

--- src/Extension.php
+++ src/Extension.php
@@ -6,6 +6,17 @@ namespace Webdevchampion\BoltformaddonExtension;
 
 use Bolt\Extension\BaseExtension;
 use Symfony\Component\Routing\Route;
+use Symfony\Component\Form\FormInterface;
+use Bolt\BoltForms\Event\BoltFormsEvents;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Bolt\BoltForms\Event\BoltFormsEvent;
+use Bolt\Storage\Entity;
+use Doctrine\ORM\EntityManager;
+use Bolt\Repository\ContentRepository;
+use Bolt\Entity\Content;
 
 class Extension extends BaseExtension
 {
@@ -47,8 +58,81 @@ class Extension extends BaseExtension
 
         $this->addTwigNamespace('boltformaddon-extension');
 
-        $this->addListener('kernel.response', [new EventListener(), 'handleEvent']);
+        //$this->addListener('kernel.response', [new EventListener(), 'handleEvent
+		
+		$this->addListener(BoltFormsEvents::POST_SET_DATA, array($this, 'populateCourses'));	
+		$this->addListener(BoltFormsEvents::SUBMIT, array($this, 'sendToVetrack'));	
+	
     }
+	
+	public function populateCourses(FormEvent $event): void
+	{
+		$data = $event->getData();
+		$event = $event->getEvent();
+		$form = $event->getForm();
+		
+		if($form->getName() === 'courses') {
+			$courseSelect = $form->get('courseselect');
+			$courseSelectOptions = $courseSelect->getConfig()->getOptions();
+			$courses = iterator_to_array($this->getQuery()->getContent('main-courses')->getCurrentPageResults());
+			$courseSelectOptions['choices'] = [];
+						
+			foreach($courses as $course) {
+				$courseSelectOptions['choices'][$course->getFieldValue('title')] = $course->getFieldValue('slug');	
+			}
+
+			$form->add('courseselect', ChoiceType::class, $courseSelectOptions);
+		}					
+	}
+	
+    public function sendToVetrack(FormEvent $event): void
+    {
+		$data = $event->getData();
+		$event = $event->getEvent();
+		$form = $event->getForm();
+		
+		if($form->getName() === 'courses') {
+			$sClie_Surname = $data['last'];
+			$sClie_Given = $data['first'];
+			$xsdClie_DOB = Date('Y-m-d', strtotime($data['dob']));
+
+			$file = fopen("test.txt","w");
+			//fwrite($file,"Hello World. Testing13".$sClie_Given.'/'.$sClie_Surname.'/'.$xsdClie_DOB);
+			
+			if($form->isSubmitted() && $form->isValid()) {
+				$VETAPIUrl = "https://trainerportal.org.au/VETtrakAPI/VT_API.asmx?wsdl";
+				 
+				$Client = new \SoapClient($VETAPIUrl);
+				$Client->TAuthenticate = $Client->API_Handshake();
+
+				// Validate client and Get Token
+				$Credentials = new \stdClass; 
+				$Credentials->sUsername = "mahmed";
+				$Credentials->sPassword = "manzoor99";
+				$Client->TAuthenticate = $Client->ValidateClient($Credentials);
+
+				// Add student to Vettrak Database
+				$GetTokenObject = new \stdClass;               
+				$GetTokenObject->sToken = $Client->TAuthenticate->ValidateClientResult->Token;
+				$GetTokenObject->sClie_Surname = $sClie_Surname;
+				$GetTokenObject->sClie_Given = $sClie_Given;
+				$GetTokenObject->xsdClie_DOB = $xsdClie_DOB; //'2021-11-28T16:30:09.000';
+				$GetTokenObject->divisionId = 0;
+
+				$Client->TAuthClie = $Client->AddClientAfterCheck($GetTokenObject);
+
+				$ReturnGiven = $Client->TAuthClie->AddClientAfterCheckResult->Clie->Clie_Given;
+				$ReturnSurname = $Client->TAuthClie->AddClientAfterCheckResult->Clie->Clie_Surname;
+				$ReturnCode = $Client->TAuthClie->AddClientAfterCheckResult->Clie->Clie_Code;
+				$ReturnStatus = $Client->TAuthClie->AddClientAfterCheckResult->Auth->StatusMessage;	
+				
+				fwrite($file, 'valid'. $ReturnGiven . '/' . $ReturnCode . '/' . $ReturnStatus);
+			} else {
+				fwrite($file, 'invalid');
+			}
+			fclose($file);	
+		}
+	}	
 
     /**
      * Ran automatically, if the current request is from the command line (CLI).
